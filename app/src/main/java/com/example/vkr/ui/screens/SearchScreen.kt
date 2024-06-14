49b546bf6c02624e22d9f23f09a4db3338cc902f
BumBoks16 <bumbooks16@gmail.com>
2024-06-15 01:24:21 +0300

    Init project

--- app/src/main/java/com/example/vkr/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/example/vkr/ui/screens/SearchScreen.kt
@@ -3,6 +3,7 @@ package com.example.vkr.ui.screens
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,8 +13,12 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,16 +33,66 @@ import com.example.vkr.ui.theme.Beige
 import com.example.vkr.MainViewModel
 import com.example.vkr.R
 import com.example.vkr.entity.Record
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
     val search = remember { mutableStateOf("") }
     val record = mainViewModel.records
 
-    Column(modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top) {
+    val state = rememberTimePickerState()
+    val openTime = remember {
+        mutableStateOf((false))
+    }
 
+    val snackState = remember { SnackbarHostState() }
+    val snackScope = rememberCoroutineScope()
+    SnackbarHost(hostState = snackState, Modifier)
+    val openDialog = remember { mutableStateOf(false) }
+    // TODO demo how to read the selected date from the state.
+    if (openDialog.value) {
+        val datePickerState = rememberDatePickerState()
+        val confirmEnabled = remember {
+            derivedStateOf { datePickerState.selectedDateMillis != null }
+        }
+        DatePickerDialog(
+            onDismissRequest = {
+                // Dismiss the dialog when the user clicks outside the dialog or on the back
+                // button. If you want to disable that functionality, simply use an empty
+                // onDismissRequest.
+                openDialog.value = false
+            },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        openTime.value = true
+                        openDialog.value = false
+                        snackScope.launch {
+                            snackState.showSnackbar(
+                                "Selected date timestamp: ${datePickerState.selectedDateMillis}"
+                            )
+                        }
+                    },
+                    enabled = confirmEnabled.value
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = { openDialog.value = false }) { Text("Cancel") }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
         Surface(
             modifier = Modifier
                 .fillMaxWidth(),
@@ -83,12 +138,14 @@ fun SearchScreen(navController: NavController, mainViewModel: MainViewModel = hi
         Log.d("zxc", "SearchScreen:${record.value}")
         LazyColumn {
             //val examplelist = listOf(com.example.vkr.entity.Record(1,"","","",5))
-            items(record.value){ item ->
-                Column(modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .background(color = Beige, shape = RoundedCornerShape(16.dp))
-                    .padding(8.dp)) {
+            items(record.value) { item ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                        .background(color = Beige, shape = RoundedCornerShape(16.dp))
+                        .padding(8.dp)
+                ) {
                     Row {
                         Image(
                             painter = painterResource(id = R.drawable.noprofileimg),
@@ -101,23 +158,47 @@ fun SearchScreen(navController: NavController, mainViewModel: MainViewModel = hi
                             Text(item.description)
                         }
                     }
-                    Text("Провожу обучение по дисциплинам: ${item.keywords}")
+                    Text(
+                        "Провожу обучение по дисциплинам: ${item.keywords}",
+                        modifier = Modifier.clickable(
+                            onClick = {openDialog.value = true})
+                    )
                     Text("Стаж: ${item.experienceYears} лет")
                 }
+
             }
         }
-
     }
+    if (openTime.value == true){
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top){
+            TimePicker(
+                state = state,
+                modifier = Modifier.padding(16.dp),
+
+            )
+            Button(onClick = { openTime.value = false }) {
+                Text(text = "Подтвердить")
+            }
+        }
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun SearchScreenPreview() {
-    val item = Record(1,"qwerwqerqwer","eqwrwqerqwer","qwerqwerweqr",5)
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .background(color = Beige, shape = RoundedCornerShape(16.dp))
-        .padding(8.dp)) {
+    val item = Record(1, "qwerwqerqwer", "eqwrwqerqwer", "qwerqwerweqr", 5)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Beige, shape = RoundedCornerShape(16.dp))
+            .padding(8.dp)
+    ) {
         Row {
             Image(
                 painter = painterResource(id = R.drawable.noprofileimg),
@@ -130,8 +211,8 @@ fun SearchScreenPreview() {
                 Text(item.description)
             }
         }
-            Text("Провожу обучение по дисциплинам: ${item.keywords}")
-            Text("Стаж: ${item.experienceYears} лет")
+        Text("Провожу обучение по дисциплинам: ${item.keywords}")
+        Text("Стаж: ${item.experienceYears} лет")
     }
 }
 
